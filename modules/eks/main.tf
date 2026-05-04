f679d2ec74969b0b2eb9e38eb60be895ba6e7371
Rishu <rs139323@gmail.com>
2026-05-04 22:29:45 +0530

    nothing

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,8 +1,8 @@
-# resource "aws_eks_cluster" "eks" {
-#   name     = "rishu-eks-cluster"
-#   role_arn = aws_iam_role.eks_cluster_role.arn
+resource "aws_eks_cluster" "eks" {
+  name     = "rishu-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
-#   vpc_config {
-#     subnet_ids = aws_subnet.public[*].id
-#   }
-# }
+  vpc_config {
+    subnet_ids = aws_subnet.public[*].id
+  }
+}
